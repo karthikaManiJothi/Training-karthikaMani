4c48ea193414c24ecec1a55aa8ac0fc72aeb4bd5
karthikaMani <84615146+karthikaManiJothi@users.noreply.github.com>
2021-05-26 22:07:21 +0530

    adding ErrorDemo5.java file

---
+++ Training-day-7/Hands-on/ErrorDemo5.java
@@ -0,0 +1,24 @@
+package com.onebill.trainig.errors;
+
+public class ErrorDemo5 {
+
+	public static void main(String[] args) {
+		int[] numb = {4,8,16,32,64,128,256,512};
+        int[] demo= {2,0,4,4,0};
+        try {
+        for(int i=0;i<numb.length;i++) {
+        	try {
+        		
+        		System.out.println(numb[i]+"/"+demo[i]+" is "+numb[i]/demo[i]);
+        		
+        	}catch(ArithmeticException e ) {
+        		System.out.println("Cannot be divided by zero");
+        	}
+        }
+        }catch(ArrayIndexOutOfBoundsException e) {
+        	System.out.println("No matching denominator found");
+        	System.out.println("Program terminated");
+        }
+	}
+
+}
