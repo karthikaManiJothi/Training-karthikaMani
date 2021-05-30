cb9c991e28b9cf826eac249aba1d915213d1ac4e
karthikaMani <84615146+karthikaManiJothi@users.noreply.github.com>
2021-05-30 21:55:48 +0530

    MainClass.java file

---
+++ Training-day-10/Handson/MainClass.java
@@ -0,0 +1,14 @@
+package com.onebill.training.day10.handson;
+
+public class MainClass extends ClassC implements InterfaceA,InterfaceB {
+
+	public static void main(String[] args) {
+       MainClass m =new MainClass();
+       m.show();
+	}
+	
+	/*public void show() {
+	   	 System.out.println("Inside Main Class");
+	    }*/
+
+}
